07d6df457b2b8c1bb44a29cfd67a5badfc90789d
I-Husillos <ivanhuslabra@gmail.com>
2025-10-30 11:48:49 +0100

    Definición de los atributos y de los metodos de la clase Artículo y Bebida

--- model/Bebida.php
+++ model/Bebida.php
@@ -1,2 +1,12 @@
 <?php
 // Clase Bebida que hereda de Artículo
+
+class Bebida extends Articulo {
+    public $tamaño;
+
+    public function __construct($nombre, $precio, $disponibilidad, $categoria, $tamaño, $temperatura) {
+        parent::__construct($nombre, $precio, $disponibilidad, $categoria);
+        $this->tamaño = $tamaño;
+    }
+}
+?>
